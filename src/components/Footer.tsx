e19310a461c803394e989f57f2cb3e597c66f906
NOUSHEEN ATIF <161060487+codewithhoney24@users.noreply.github.com>
2024-11-21 11:04:16 -0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p>
+        < p   className='footer-para'>
             &copy; 2024,Home Decor. All Rights Reserved.
         </p>
     </footer>
